b05e95ce3fc25f3d903dec0811b1ab47ec557f1c
Aisling Boner <55714619+AislingBoner@users.noreply.github.com>
2020-02-12 18:19:43 +0000

    Add files via upload

---
+++ aislingBoner.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+#
+##############################################################
+#Script Name	:	Project/projectQ2.2
+#Description	:	A Script which runs several commands.
+#Author		:	Aisling Boner
+#Student Number	:	G00376422
+#Email		:	aislingboner@gmail.com
+##############################################################
+
+
+echo 
+echo "#########################################"
+echo "Research Project for Q2.2 "
+echo "#########################################"
+echo 
+echo 
+
+
+
+echo 
+echo "-----------------------------------------"
+echo "Current Date & Time:  "
+echo "-----------------------------------------"
+echo 
+
+
+	date 
+
+echo
+echo 
+echo "-----------------------------------------"
+echo "Host IP address:  "
+echo "-----------------------------------------" 
+echo 
+
+
+	hostname 
+
+echo
+echo "-----------------------------------------"
+echo "Computer Architecture Specs: "
+echo "-----------------------------------------" 
+echo 
+
+
+	arch 
+
+echo 
+echo 
+echo "-----------------------------------------"
+echo "Computer Architecture Specs: "
+echo "-----------------------------------------"  
+echo 
+
+	uname -a 
+
+echo 
+echo 
+echo "-----------------------------------------"
+echo "Current Activity Length Period: "
+echo "-----------------------------------------"  
+echo
+
+
+	uptime 
+
+echo 
+echo 
+echo "-----------------------------------------"
+echo "Current User's Username: "
+echo "-----------------------------------------" 
+echo 
+
+	whoami 
+
+echo 
+echo
+echo "-----------------------------------------"
+echo "All Users Who are Logged in: "
+echo "-----------------------------------------" 
+echo 
+
+	who 
+
+echo
+echo 
+echo "-----------------------------------------"
+echo "User Details: "
+echo "-----------------------------------------" 
+echo
+
+
+	finger 
+
+echo 
+echo 
+echo "-----------------------------------------"
+echo "Login,Activily & Computer Usage Summary: "
+echo "-----------------------------------------" 
+echo 
+
+	w
+
+echo 
+echo 
+echo "-----------------------------------------"
+echo "Top Proccesses Currently Running: "
+echo "-----------------------------------------" 
+echo 
+
+	top	
+	#(you may need to press q to quit) 
+
+echo 
+echo 
+echo "-----------------------------------------"
+echo "History of Commands Inputted by User: "
+echo "-----------------------------------------" 
+echo
+
+	history 
+
+echo "#########################################"
+echo "#########################################"
+
